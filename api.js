c299f142a391aa6ca67dc2e0433f04a999627d40
AkibRiad <akibriad652@gmail.com>
2024-10-15 15:42:39 +0600

    ajaira jinis added

---
+++ api.js
@@ -0,0 +1,38 @@
+// var 
+// let notun vabe man dite parbo
+//  const notun vabe man dite parbo na
+
+// function addNumbers(num1,num2){
+//     const add = num1 + num2;
+//     console.log(add);
+    
+// }
+// const sum = addNumbers(50,25);
+
+
+// function myName(fst = '', lst = ''){
+//     const full = fst + ' ' + lst;
+//     console.log(full);
+// }
+
+
+
+// const getArray = myName();
+
+
+// function myArray(obj = []){
+//     const full = obj;
+//     console.log(full);
+// }
+
+
+
+// const array = myArray();
+function myName(numbs = {}){
+    const full = numbs;
+    console.log(full);
+}
+
+
+
+const object = myName({akib, engineer});
