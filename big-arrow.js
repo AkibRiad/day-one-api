94940a26f8b46c6dba34e5997e07eb805920064e
AkibRiad <akibriad652@gmail.com>
2024-10-15 22:46:29 +0600

    spread understanding

--- big-arrow.js
+++ big-arrow.js
@@ -1,5 +1,18 @@
   const difference = (a,b) => a - b;
 
-
-
   console.log(difference(100,1));
+
+
+
+  const getAge = person => person.age;
+
+ const student = {name: 'akib', age: 25};
+  console.log(getAge(student));
+
+  const getThird = x => x[2];
+
+//  const arr = ;
+  console.log(getThird([5,6,50,8]));
+
+
+
