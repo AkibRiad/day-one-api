2074d68da2b85010ba695a3b399eade38d929216
AkibRiad <akibriad652@gmail.com>
2024-10-16 15:34:55 +0600

    destructure understand

--- destructure.js
+++ destructure.js
@@ -4,8 +4,8 @@ const myInfo = {
     occupation:'Student'
 }
 
-console.log(typeof myInfo.occupation);
-console.log(typeof myInfo.age);
+// console.log(typeof myInfo.occupation);
+// console.log(typeof myInfo.age);
 
 const myInfo2 = {
     name: 'akib riad',
@@ -16,7 +16,22 @@ const myInfo2 = {
 const {age}= myInfo2;
 const {name}= myInfo2;
 
-console.log(age)
-console.log(name)
+// console.log(age)
+// console.log(name)
 
 
+// array destructuring 
+
+const getNumber = [10,30,20,50,40];
+
+const [x,y,z,w,s] = getNumber;
+// console.log(s);
+
+// double them
+function doubleThem(a,b){
+    return [a*2,b*2];
+}
+
+const [aaaa,b] = doubleThem(9,10)
+console.log(doubleThem(5,7))
+console.log(aaaa,b)
