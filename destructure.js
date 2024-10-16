e28fc08431c07ce510cd9a89a10a2b39853257e1
AkibRiad <akibriad652@gmail.com>
2024-10-16 15:21:00 +0600

    spread & destructure understanding

---
+++ destructure.js
@@ -0,0 +1,22 @@
+const myInfo = {
+    name: 'akib riad',
+    age: 25,
+    occupation:'Student'
+}
+
+console.log(typeof myInfo.occupation);
+console.log(typeof myInfo.age);
+
+const myInfo2 = {
+    name: 'akib riad',
+    age: 25,
+    occupation:'Student'
+}
+
+const {age}= myInfo2;
+const {name}= myInfo2;
+
+console.log(age)
+console.log(name)
+
+
--- r.js
+++ r.js
@@ -1,6 +1,6 @@
 const getAge = (person) => person[3];
 
-console.log(getAge([1,2,3,4,5,6,7]));
+// console.log(getAge([1,2,3,4,5,6,7]));
 
 
 
@@ -8,5 +8,15 @@ const a = [1,2,3,4,5];
 const b = [...a];
 
 b.push(60);
-console.log(a);
-console.log(...b);
+// console.log(a);
+// console.log(...b);
+
+
+
+const getMax = () => Math.max(30,20,50,60,800);
+
+const numbers = [30,50,20,10,5,45];
+
+console.log(getMax());
+console.log((Math.max(...numbers)));
+
